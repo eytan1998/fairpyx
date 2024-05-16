bdace5db3ecad50f7471742be8ef20f2fc0ee679
eytan1998 <eytan1998@gmail.com>
2024-05-16 13:18:23 +0300

    setup

---
+++ fairpyx/algorithms/ps_lottery.py
@@ -0,0 +1,8 @@
+"""
+An implementation of algorithm in:
+2020, Aziz, Simultaneously achieving ex-ante and ex-post fairness
+https://arxiv.org/abs/2004.02554
+
+Author: Eitan Ankri, Or Kfir
+Date: 2020-04-02
+"""
